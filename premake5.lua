3c6686e8f4a2ddb826aeffd399c10649cdd45a82
SirDisco <josephwalker2010@hotmail.co.uk>
2021-01-11 12:21:51 +0000

    Added intial premake5.lua file to generate makefiles

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+workspace "Connect 4 With AI"
+    configurations { "Debug", "Release" }
+    architecture "x64"
+
+project "Connect 4"
+    kind "ConsoleApp"
+    language "C++"
+
+    location "%{prj.name}"
+
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin/%{cfg.buildcfg}-obj"
+
+    files
+    {
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src"
+    }
+
+    filter "configurations:Debug"
+        symbols "On"
+        defines "DEBUG"
+    
+    filter "configurations:Release"
+        optimize "On"
+        defines "NDEBUG"
